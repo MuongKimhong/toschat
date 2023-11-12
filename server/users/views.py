fb938c6972c82f6d60bd6001b72ab167a9ae010f
MuongKimhong <muongkimhong086@gmail.com>
2023-11-12 21:51:40 +1100

    SignIn api

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password, check_password
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+def get_token(user):
+    refresh_token = RefreshToken.for_user(user) 
+    access_token  = refresh_token.access_token
+
+    data = {
+        'user': user.serialize(),
+        'refresh_token': str(refresh_token),
+        'access_token' : str(access_token)
+    }
+    return data
+
+
+class SignIn(APIView):
+    permission_classes = [ AllowAny ]
+
+    def post(self, request):
+        try:
+            user = User.objects.get(username=request.data["username"])
+        except User.DoesNotExist:
+            return Response({"error": True}, status=400)
+
+        if check_password(request.data["password"], user.password) is False:
+            return Response({"error": True}, status=400)
+
+        return Response(get_token(user), status=200)
