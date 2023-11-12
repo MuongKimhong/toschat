ddda4ffc480cf0988b57af8c415314338a1d17fd
MuongKimhong <muongkimhong086@gmail.com>
2023-11-12 21:47:12 +1100

    create custom user model

--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.id}"
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "created_date": date_format(self.created_date),
+        }
