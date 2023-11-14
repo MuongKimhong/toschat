5cdf52b87d1473065d29ff2935a8e89e0d701c37
MuongKimhong <muongkimhong086@gmail.com>
2023-11-14 17:15:37 +1100

    show err in sign in screen

--- src/toschat/screens/signin_screen/signin.py
+++ src/toschat/screens/signin_screen/signin.py
@@ -2,6 +2,16 @@ from textual.widgets import Static, Header, Input, Button
 from textual.containers import Container, Horizontal, Vertical
 from textual.app import ComposeResult
 from textual.screen import Screen
+from pathlib import Path
+import requests
+import time
+import os
+
+from ..variables import SERVER_BASE_URL
+
+
+def sign_in(username, password):
+    pass
 
 
 class SignInScreen(Screen):
@@ -10,6 +20,7 @@ class SignInScreen(Screen):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Static("Sign In", classes="sign-in-text")
+        yield Static("Username or password is incorrect", id="error-text")
         yield Input(placeholder="Username", id="username-input")
         yield Input(placeholder="Password", password=True, id="password-input")
         yield Container(
@@ -29,3 +40,19 @@ class SignInScreen(Screen):
             self.app.install_screen(SignUpScreen, "signup")
             self.app.switch_screen("signup")
             self.app.uninstall_screen("signin")
+
+        elif event.button.id == "signin-btn":
+            self.query_one("#error-text").styles.visibility = "hidden"
+            username = self.query_one("#username-input").value
+            password = self.query_one("#password-input").value
+
+            if (username.strip() == "") or (password.strip() == ""):
+                self.query_one("#error-text").styles.visibility = "visible"
+                self.query_one("#username-input").styles.margin = (1, 12, 1, 12)
+            else:
+                home_dir = Path.home()
+
+                # create an empty json file to store credential
+                if os.path.exists(f"{home_dir}/toschat_cred.json") is False:
+                    open(f"{home_dir}/toschat_cred.json", "a").close()                
+
---
+++ src/toschat/screens/variables.py
@@ -0,0 +1,4 @@
+
+SERVER_BASE_URL = "http://localhost:8000/"
+
+WEB_SOCKET_URL = "127.0.0.1:59000"
