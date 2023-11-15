b09f6f0938daa853622ca13dc262407206058d18
MuongKimhong <muongkimhong086@gmail.com>
2023-11-15 13:50:22 +1100

    add return type hint to funcs

--- src/toschat/screens/home_screen/home.py
+++ src/toschat/screens/home_screen/home.py
@@ -2,6 +2,8 @@ from textual.widgets import Static, Header, Input, Button
 from textual.containers import Container, Horizontal, ScrollableContainer
 from textual.app import ComposeResult
 from textual.screen import Screen
+from rich.segment import Segment
+from textual.strip import Strip
 from pathlib import Path
 import requests
 import time
@@ -11,7 +13,7 @@ import os
 from ..variables import SERVER_BASE_URL   
 
 
-def read_cred_file():
+def read_cred_file() -> dict:
     path = f"{Path.home()}/toschat_cred.json"
 
     with open(path, "r") as cred_file:
@@ -20,13 +22,14 @@ def read_cred_file():
     return credential  
 
 
-def get_all_users(current_user_id, access_token):
+def get_all_users(current_user_id: int, access_token: str) -> list:
+
     url = f"{SERVER_BASE_URL}api-users/list-all-users/"
     params = {"current_user_id": current_user_id}
     headers = {"Authorization": f"Bearer {access_token}"}
 
     response = requests.get(url, params=params, headers=headers)
-    response = json.loads(response.text)
+    response: dict = json.loads(response.text)
 
     return response["users"]
 
@@ -35,15 +38,18 @@ class HomeScreen(Screen):
     CSS_PATH = "home.tcss"
     credential = None
     all_users = []
+    all_usernames = []
 
     def compose(self) -> ComposeResult:        
         self.credential = read_cred_file()
         self.all_users = get_all_users(self.credential["user"]["id"], self.credential["access_token"])
+        self.all_usernames = [Static(user["username"], classes="username-text") for user in self.all_users]
         
         yield Container(
             Horizontal(Button("Logout", id="logout")),
             id="top-container"
         )
+        yield ScrollableContainer(*self.all_usernames, id='user-list-container')
     
     def logout(self):
         os.remove(f"{Path.home()}/toschat_cred.json")
