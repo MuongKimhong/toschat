7f1eb387be500a5fc240bb3e120fefb895a7b8b9
MuongKimhong <muongkimhong086@gmail.com>
2023-11-15 13:50:00 +1100

    create create_dummy_users func

--- server/users/tests.py
+++ server/users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.contrib.auth.hashers import make_password
+from users.models import User
 
-# Create your tests here.
+
+def create_dummy_users(number_of_user: int) -> None:
+    for i in range(number_of_user):
+        print(f"Creating user {i}")
+        user = User.objects.create(
+            username=f"user{i}",
+            password=make_password("12345")
+        )
+
+    print("Finished") 
+
