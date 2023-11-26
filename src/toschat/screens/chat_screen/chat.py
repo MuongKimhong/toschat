6ed36d25896704439ea8539e88602fd1e5b3aa0c
MuongKimhong <muongkimhong086@gmail.com>
2023-11-26 14:09:42 +1100

    get message in chatroom without problems

--- src/toschat/screens/chat_screen/chat.py
+++ src/toschat/screens/chat_screen/chat.py
@@ -202,22 +202,123 @@ class NavbarWidget(Static):
             self.logout()
 
 
+class MessageWidget(Static):
+    def compose(self) -> ComposeResult:
+        '''
+        renderable arg will be name-messagetextblahblah 
+        renderable_split[0] is name
+        renderable_split[1] is messagetextblahblahblah
+        '''
+        renderable_split = self.renderable.split("-")
+        yield Static(f"{renderable_split[0]}: {renderable_split[1]}", classes="username")
+
+
+class MessageAreaWidget(Widget):
+    class Receive(Message):
+        def __init__(self, event) -> None:
+            self.event = event
+            super().__init__()
+    
+    def __init__(self, websocket, credential) -> None:
+        self.websocket = websocket
+        self.credential = credential
+        self.list_view = ListView(*[], id="messages-list-view")
+        super().__init__()
+
+    def get_messages_in_chatroom(self) -> list:
+        url = f"{SERVER_BASE_URL}api-chats/get-messages/"
+        params = {"chatroom_id": self.credential['selected_chatroom_id']}
+        headers = {"Authorization": f"Bearer {self.credential['access_token']}"}
+
+        response = requests.get(url, params=params, headers=headers)
+        response: dict = json.loads(response.text)
+
+        return response["messages"]
+
+    def compose(self) -> ComposeResult: 
+        messages = self.get_messages_in_chatroom()
+        yield Container(self.list_view, id="messages-container")
+
+        for message in messages:
+            message_widget = MessageWidget(f"{message['sender']['username']}-{message['text']}")
+            self.list_view.append(
+                ListItem(message_widget, classes="list-item")
+            ) 
+        self.list_view.scroll_end()
+        self.websocket_receive()
+        
+    @work(exclusive=True, thread=True)
+    def websocket_receive(self):
+        while True:
+            try:
+                event = self.websocket.receive()
+                if event[0] == "newmessage":
+                    self.post_message(self.Receive(event[1]))
+            except:
+                break
+
+
 class ChatScreen(Screen):
     CSS_PATH = "chat.tcss" 
-
+    websocket = None
+    credential = None
+    list_view = ListView(*[], id="messages-list-view")
 
     def compose(self) -> ComposeResult:
         yield NavbarWidget()
+        yield Container(self.list_view, id="messages-container")
+
+
+    def clear_data(self):
+        self.websocket = None
+        self.credential = None
+        self.list_view.clear()
+
+    
+    def get_messages_in_chatroom(self):
+        url = f"{SERVER_BASE_URL}api-chats/get-messages/"
+        params = {"chatroom_id": self.credential['selected_chatroom_id']}
+        headers = {"Authorization": f"Bearer {self.credential['access_token']}"}
+
+        response = requests.get(url, params=params, headers=headers)
+        messages: list = json.loads(response.text)["messages"]
+
+        messages_widget = []
+
+        for message in messages:
+            message_widget = MessageWidget(f"{message['sender']['username']}-{message['text']}")
+            self.list_view.append(ListItem(message_widget, classes="list-item"))
+
+        self.list_view.scroll_end()
 
 
     def on_screen_resume(self, event: events.ScreenResume) -> None:
 
         # update username on navbar based on cred_file
         with open(f"{Path.home()}/toschat_cred.json", "r") as cred_file:
-            another_username = json.load(cred_file)["selected_username_to_message"]
+            self.credential = json.load(cred_file)
+            another_username = self.credential["selected_username_to_message"]
             self.query_one("#another-username").update(another_username)
+
+        # connect websocket
+        self.websocket = socketio.SimpleClient()
+        self.websocket.connect("http://localhost:3000")
+
+        # get messages
+        self.get_messages_in_chatroom()
 
 
     def on_screen_suspend(self, event: events.ScreenSuspend) -> None:
-        pass
-    
+        # disconnect websocket
+        self.websocket.disconnect()
+        
+        # cancel worker
+        try:
+            current_worker = get_current_worker()
+            if current_worker.is_running:
+                current_worker.cancel()
+        except NoActiveWorker:
+            pass
+
+        # clear data
+        self.clear_data()
