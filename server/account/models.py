5a9f5dcfd5a00695555da73f49e772e2012d3e19
MuongKimhong <muongkimhong086@gmail.com>
2024-03-15 13:57:14 +1100

    implemented models

--- server/account/models.py
+++ server/account/models.py
@@ -1,3 +1,27 @@
+from typing import Dict
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=200, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.username} - {self.id}"
+
+    def serialize(self) -> Dict[str, str]:
+        return {"id": str(self.id), "username": self.username}
+
+
+class UserContact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    contact = models.ForeignKey(User, related_name="contact", on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_ad=True)
+
+    def serialize(self) -> Dict[str, Dict[str, str]]:
+        return {
+            "user": self.user.serialize(),
+            "contact": self.contact.serialize()
+        }
--- server/chat/models.py
+++ server/chat/models.py
@@ -1,3 +1,29 @@
+from typing import Dict, Union
+
 from django.db import models
 
-# Create your models here.
+from account.models import User, UserContact
+
+
+class ChatRoom(models.Model):
+    members = models.ManyToManyField(User)
+    created_date = models.DateTimeField(auto_now_ad=True)
+
+    def serialize(self) -> Dict[str, Union[str, list[Dict[str, str]]]]:
+        return {
+            "id": str(self.id),
+            "members": [member.serialize() for member in self.members.all()]
+        }
+
+
+class Message(models.Model):
+    chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_ad=True)
+
+    def serialize(self):
+        return {
+            "sender": self.sender.serialize(),
+            "message": {"id": str(self.id), "text": self.text}
+        }
