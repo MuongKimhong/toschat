76e608de344c241d169d21e5f4a111017594d939
MuongKimhong <muongkimhong086@gmail.com>
2024-03-18 21:21:12 +1100

    ttab for websocket dir

--- dev_start.sh
+++ dev_start.sh
@@ -3,6 +3,8 @@
 
 git pull origin master
 
+ttab "cd $PWD/websocket/; npm start"
+
 ttab "cd $PWD; source .venv/bin/activate; python3 server/manage.py runserver" 
 
 ttab "cd $PWD; source .venv/bin/activate; textual console"
