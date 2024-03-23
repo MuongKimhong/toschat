20ab7dc753c60bdcc401901d064abd03d1487fac
MuongKimhong <muongkimhong086@gmail.com>
2024-03-23 14:13:25 +1100

    new namespace for updating online status

--- websocket/index.js
+++ websocket/index.js
@@ -13,8 +13,19 @@ server.listen(devPort, () => {
     console.log("websocket server started");
 })
 
+// default namespace "/" for handling chat message
 socketIO.on("connection", (socket) => {
     socket.on("send-message", (message) => {
         socket.broadcast.emit("new-message", message);
+    })
+})
+
+// namespace "/onlineStatus" for handling user online & offline status
+const onlineStatus = socketIO.of("/onlineStatus");
+onlineStatus.on("connection", (socket) => {
+    socket.on("update-online-status", (data) => {
+        // data content : {sender_name: "somename", status: "online"}
+        // data content : {sender_name: "somename", status: "offline"}
+        socket.broadcast.emit("online-status-update", data);
     })
 })
